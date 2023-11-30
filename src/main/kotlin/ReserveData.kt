2f48574218bd8bdd3a032492cc639aa1c88e9129
YS <etgt777@naver.com>
2023-12-01 00:14:30 +0900

    sortList Complete

--- src/main/kotlin/ReserveData.kt
+++ src/main/kotlin/ReserveData.kt
@@ -2,12 +2,13 @@ class ReserveData {
     var reserveList = mutableListOf<List<String>>()
 
    fun reserveCheck () {
-        reserveList.forEach{ println("예약자 : ${it[0]}   방번호 : ${it[1]}   체크인 : ${it[2]}   체크아웃 : ${it[3]}") }
+        reserveList.forEach{ println("${reserveList.indexOf(it) + 1}. 예약자 : ${it[0]}   방번호 : ${it[1]}   체크인 : ${it[2]}   체크아웃 : ${it[3]}") }
     }
 
     fun sortCheck () {
-        reserveList.sortedWith(compareBy<List<String>> {it[2]}.thenBy { it[3] }).toMutableList().forEach{
-            println("예약자 : ${it[0]}   방번호 : ${it[1]}   체크인 : ${it[2]}   체크아웃 : ${it[3]}")
+        var sortList = reserveList.sortedWith(compareBy<List<String>> {it[2]}.thenBy { it[3] }).toMutableList()
+        sortList.forEach{
+            println("${sortList.indexOf(it) + 1}. 예약자 : ${it[0]}   방번호 : ${it[1]}   체크인 : ${it[2]}   체크아웃 : ${it[3]}")
         }
     }
 
